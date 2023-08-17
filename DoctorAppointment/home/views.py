97824e0fef7f28aea335cd3fc57dd410dd28a415
Developer <ijasahmediku987@gmail.com>
2023-08-17 22:28:08 +0530

    doctorsModel

--- DoctorAppointment/home/views.py
+++ DoctorAppointment/home/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Departments
 # Create your views here.
 def index(request):
-    number={
-        'num1':[0,1,2,3,4,5,6,7,8,9]
-    }
-    return render(request,'index.html',number)
+    
+    return render(request,'index.html')
 
 def about(request):
     return render(request,'about.html')
@@ -20,4 +19,7 @@ def contact(request):
     return render(request,'contact.html')
 
 def department(request):
-    return render(request,'department.html')
+    dept_dict={
+        'dept':Departments.objects.all()
+    }
+    return render(request,'department.html',dept_dict)
